e6377c79695cc7b240063103664b15e586e3f109
FelipeVSReis <felipe.vitow@gmail.com>
2018-06-26 14:40:31 -0300

    Organizacao dos javascripts

---
+++ starter-template/js/Register.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+
+	$('.sidenav').sidenav();
+		
+	$(document).ready(function(){
+		$('.datepicker').datepicker();
+	});
+	
+	$(document).ready(function(){
+		$('select').formSelect();
+	});
+		
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
